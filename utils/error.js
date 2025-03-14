b757195c43c9a2bf52cbf68cb8527ed0b09ad6ab
Fumnanya Akupue <107567601+vanessaakupue@users.noreply.github.com>
2025-03-14 10:54:42 +0100

    Update error.js

--- utils/error.js
+++ utils/error.js
@@ -2,8 +2,12 @@ export const getErrorMessage = (errorCode) => {
   switch (errorCode) {
     case 'auth/invalid-email':
       return 'Invalid email address. Please check your email and try again.';
+    case 'auth/user-not-found':
+      return 'No account found with this email. Please sign up.';
     case 'auth/email-already-in-use':
       return 'This email is already in use. Please use a different email.';
+    case 'auth/wrong-password':
+      return 'Incorrect password. Please try again.';
     case 'auth/invalid-credential':
       return 'Invalid email or password. Please check your credentials and try again.';
     default:
